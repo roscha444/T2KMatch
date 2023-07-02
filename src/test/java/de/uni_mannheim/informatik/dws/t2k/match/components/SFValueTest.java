2f5701cda519d7ec776efa1488c3fa06f64b61c6
Robin Schumacher <info@robin-schumacher.como>
2023-07-02 21:41:30 +0200

    refactor tests

--- src/test/java/de/uni_mannheim/informatik/dws/t2k/match/components/SFValueTest.java
+++ src/test/java/de/uni_mannheim/informatik/dws/t2k/match/components/SFValueTest.java
@@ -1,5 +1,7 @@
 package de.uni_mannheim.informatik.dws.t2k.match.components;
 
+import de.uni_mannheim.informatik.dws.t2k.match.components.similarityflooding.value.SFValueBasedMatchingKB2WB;
+import de.uni_mannheim.informatik.dws.t2k.match.components.similarityflooding.value.SFValueBasedMatchingWB2KB;
 import de.uni_mannheim.informatik.dws.t2k.match.data.KnowledgeBase;
 import de.uni_mannheim.informatik.dws.t2k.match.data.MatchableTableColumn;
 import de.uni_mannheim.informatik.dws.t2k.match.data.MatchableTableRow;
@@ -22,14 +24,19 @@ import java.util.Map;
 import java.util.Set;
 import junit.framework.TestCase;
 
+/**
+ * Test for the SF Value based matcher {@link SFValueBasedMatchingWB2KB} and {@link SFValueBasedMatchingKB2WB}
+ *
+ * @author Robin Schumacher (info@robin-schumacher.com)
+ */
 public class SFValueTest extends TestCase {
 
-    public void test() throws Exception {
+    public void test_WB_2_KB() throws Exception {
         // prepare
 
         MatchingGoldStandard schemaGs = null;
         // load schema gold standard
-        File schemaGsFile = new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/gs_property.csv");
+        File schemaGsFile = new File("src/test/resources/sfValueTest/gs_property.csv");
         if (schemaGsFile.exists()) {
             schemaGs = new MatchingGoldStandard();
             schemaGs.loadFromCSVFile(schemaGsFile);
@@ -37,16 +44,16 @@ public class SFValueTest extends TestCase {
         }
 
         WebTables.setDoSerialise(false);
-        WebTables wb = WebTables.loadWebTables(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/wb"), false, true, false);
+        WebTables wb = WebTables.loadWebTables(new File("src/test/resources/sfValueTest/wb"), false, true, false);
 
         KnowledgeBase kb = new KnowledgeBase();
-        SurfaceForms sf = new SurfaceForms(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/surfaceforms.txt"),
-            new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/redirects.txt"));
+        SurfaceForms sf = new SurfaceForms(new File("src/test/resources/sfValueTest/surfaceforms.txt"),
+            new File("src/test/resources/sfValueTest/redirects.txt"));
         sf.loadIfRequired();
-        IIndex index = new DefaultIndex("src\\test\\resources\\sfValueTest\\index\\");
-        KnowledgeBase.loadClassHierarchy("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/OntologyDBpedia");
+        IIndex index = new DefaultIndex("src/test/resources/sfValueTest/index");
+        KnowledgeBase.loadClassHierarchy("src/test/resources/sfValueTest/redirects.txt");
         assertNotNull(sf);
-        kb = KnowledgeBase.loadKnowledgeBase(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/kb"), index, sf);
+        kb = KnowledgeBase.loadKnowledgeBase(new File("src/test/resources/sfValueTest/kb"), index, sf);
 
         Map<Integer, Set<String>> classPerTable = new HashMap<>();
         classPerTable.put(0, new HashSet<>());
@@ -54,18 +61,18 @@ public class SFValueTest extends TestCase {
 
         Processable<Correspondence<MatchableTableColumn, MatchableTableRow>> keyCorrespondences = wb.getKeys().map(new WebTableKeyToRdfsLabelCorrespondenceGenerator(kb.getRdfsLabel()));
         MatchingEngine<MatchableTableRow, MatchableTableColumn> matchingEngine = new MatchingEngine<>();
-        CandidateSelection cs = new CandidateSelection(matchingEngine, false, index, "src\\test\\resources\\sfValueTest\\index\\", wb, kb, sf, keyCorrespondences);
+        CandidateSelection cs = new CandidateSelection(matchingEngine, false, index, "src/test/resources/sfValueTest/index", wb, kb, sf, keyCorrespondences);
         Processable<Correspondence<MatchableTableRow, MatchableTableColumn>> instanceCorrespondences = cs.run();
 
         CandidateFiltering classFilter = new CandidateFiltering(classPerTable, kb.getClassIndices(), instanceCorrespondences);
         instanceCorrespondences = classFilter.run();
 
-        SFValueBasedMatching sfValueBasedMatching = new SFValueBasedMatching(wb, kb, classPerTable, instanceCorrespondences);
-        sfValueBasedMatching.setSf(sf);
-        sfValueBasedMatching.setInstanceCorrespondences(instanceCorrespondences);
+        SFValueBasedMatchingWB2KB sfValueBasedMatchingWB2KB = new SFValueBasedMatchingWB2KB(wb, kb, classPerTable, instanceCorrespondences);
+        sfValueBasedMatchingWB2KB.setSurfaceForms(sf);
+        sfValueBasedMatchingWB2KB.setInstanceCorrespondences(instanceCorrespondences);
 
         // execute
-        Processable<Correspondence<MatchableTableColumn, MatchableTableRow>> result = sfValueBasedMatching.run();
+        Processable<Correspondence<MatchableTableColumn, MatchableTableRow>> result = sfValueBasedMatchingWB2KB.run();
 
         // validate
 
@@ -82,12 +89,12 @@ public class SFValueTest extends TestCase {
         assertEquals("openingDate", resultList.get("jahr"));
     }
 
-    public void test2() throws Exception {
+    public void test_KB_2_WB() throws Exception {
         // prepare
 
         MatchingGoldStandard schemaGs = null;
         // load schema gold standard
-        File schemaGsFile = new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/gs_property.csv");
+        File schemaGsFile = new File("src/test/resources/sfValueTest/gs_property.csv");
         if (schemaGsFile.exists()) {
             schemaGs = new MatchingGoldStandard();
             schemaGs.loadFromCSVFile(schemaGsFile);
@@ -95,15 +102,16 @@ public class SFValueTest extends TestCase {
         }
 
         WebTables.setDoSerialise(false);
-        WebTables wb = WebTables.loadWebTables(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/wb"), false, true, false);
+        WebTables wb = WebTables.loadWebTables(new File("src/test/resources/sfValueTest/wb"), false, true, false);
 
         KnowledgeBase kb = new KnowledgeBase();
-        SurfaceForms sf = new SurfaceForms(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/surfaceforms.txt"), new File("src\\test\\resources\\redirect\\redirects"));
+        SurfaceForms sf = new SurfaceForms(new File("src/test/resources/sfValueTest/surfaceforms.txt"),
+            new File("src/test/resources/sfValueTest/redirects.txt"));
         sf.loadIfRequired();
-        IIndex index = new DefaultIndex("src\\test\\resources\\sfValueTest\\index\\");
-        KnowledgeBase.loadClassHierarchy("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/OntologyDBpedia");
+        IIndex index = new DefaultIndex("src/test/resources/sfValueTest/index");
+        KnowledgeBase.loadClassHierarchy("src/test/resources/sfValueTest/redirects.txt");
         assertNotNull(sf);
-        kb = KnowledgeBase.loadKnowledgeBase(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/kb"), index, sf);
+        kb = KnowledgeBase.loadKnowledgeBase(new File("src/test/resources/sfValueTest/kb"), index, sf);
 
         Map<Integer, Set<String>> classPerTable = new HashMap<>();
         classPerTable.put(0, new HashSet<>());
@@ -111,72 +119,14 @@ public class SFValueTest extends TestCase {
 
         Processable<Correspondence<MatchableTableColumn, MatchableTableRow>> keyCorrespondences = wb.getKeys().map(new WebTableKeyToRdfsLabelCorrespondenceGenerator(kb.getRdfsLabel()));
         MatchingEngine<MatchableTableRow, MatchableTableColumn> matchingEngine = new MatchingEngine<>();
-        CandidateSelection cs = new CandidateSelection(matchingEngine, false, index, "src\\test\\resources\\sfValueTest\\index\\", wb, kb, sf, keyCorrespondences);
+        CandidateSelection cs = new CandidateSelection(matchingEngine, false, index, "src/test/resources/sfValueTest/index", wb, kb, sf, keyCorrespondences);
         Processable<Correspondence<MatchableTableRow, MatchableTableColumn>> instanceCorrespondences = cs.run();
 
         CandidateFiltering classFilter = new CandidateFiltering(classPerTable, kb.getClassIndices(), instanceCorrespondences);
         instanceCorrespondences = classFilter.run();
 
-        SFValueBasedMatching sfValueBasedMatching = new SFValueBasedMatching(wb, kb, classPerTable, instanceCorrespondences);
-        sfValueBasedMatching.setSf(sf);
-        sfValueBasedMatching.setInstanceCorrespondences(instanceCorrespondences);
-
-        // execute
-        Processable<Correspondence<MatchableTableColumn, MatchableTableRow>> result = sfValueBasedMatching.run();
-
-        // validate
-
-        evaluateSchemaCorrespondences(schemaGs, result, "sf");
-
-        HashMap<String, String> resultList = new HashMap<>();
-        for (Correspondence<MatchableTableColumn, MatchableTableRow> correspondence : result.get()) {
-            resultList.put(correspondence.getFirstRecord().getHeader(), correspondence.getSecondRecord().getHeader());
-        }
-
-        assertEquals("floorCount", resultList.get("etagen"));
-        assertEquals("rdf-schema#label", resultList.get("gebäude"));
-        assertEquals("openingDate", resultList.get("jahr"));
-        assertEquals("location", resultList.get("stadt"));
-    }
-
-    public void test3() throws Exception {
-        // prepare
-
-        MatchingGoldStandard schemaGs = null;
-        // load schema gold standard
-        File schemaGsFile = new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/gs_property.csv");
-        if (schemaGsFile.exists()) {
-            schemaGs = new MatchingGoldStandard();
-            schemaGs.loadFromCSVFile(schemaGsFile);
-            schemaGs.setComplete(true);
-        }
-
-        WebTables.setDoSerialise(false);
-        WebTables wb = WebTables.loadWebTables(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/wb"), false, true, false);
-
-        KnowledgeBase kb = new KnowledgeBase();
-        SurfaceForms sf = new SurfaceForms(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/surfaceforms.txt"),
-            new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/redirects.txt"));
-        sf.loadIfRequired();
-        IIndex index = new DefaultIndex("src\\test\\resources\\sfValueTest\\index\\");
-        KnowledgeBase.loadClassHierarchy("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/OntologyDBpedia");
-        assertNotNull(sf);
-        kb = KnowledgeBase.loadKnowledgeBase(new File("/Users/I523269/SAPDevelop/T2KMatch_2/src/test/resources/sfValueTest/kb"), index, sf);
-
-        Map<Integer, Set<String>> classPerTable = new HashMap<>();
-        classPerTable.put(0, new HashSet<>());
-        classPerTable.get(0).add("Building");
-
-        Processable<Correspondence<MatchableTableColumn, MatchableTableRow>> keyCorrespondences = wb.getKeys().map(new WebTableKeyToRdfsLabelCorrespondenceGenerator(kb.getRdfsLabel()));
-        MatchingEngine<MatchableTableRow, MatchableTableColumn> matchingEngine = new MatchingEngine<>();
-        CandidateSelection cs = new CandidateSelection(matchingEngine, false, index, "src\\test\\resources\\sfValueTest\\index\\", wb, kb, sf, keyCorrespondences);
-        Processable<Correspondence<MatchableTableRow, MatchableTableColumn>> instanceCorrespondences = cs.run();
-
-        CandidateFiltering classFilter = new CandidateFiltering(classPerTable, kb.getClassIndices(), instanceCorrespondences);
-        instanceCorrespondences = classFilter.run();
-
-        SFValueBasedMatchingInverse sfValueBasedMatching = new SFValueBasedMatchingInverse(wb, kb, classPerTable, instanceCorrespondences);
-        sfValueBasedMatching.setSf(sf);
+        SFValueBasedMatchingKB2WB sfValueBasedMatching = new SFValueBasedMatchingKB2WB(wb, kb, classPerTable, instanceCorrespondences);
+        sfValueBasedMatching.setSurfaceForms(sf);
         sfValueBasedMatching.setInstanceCorrespondences(instanceCorrespondences);
 
         // execute
