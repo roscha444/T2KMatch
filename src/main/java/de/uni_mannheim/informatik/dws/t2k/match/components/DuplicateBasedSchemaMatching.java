263a57d70f29bdd80932e6465816bb2bf194dd5e
Robin Schumacher <info@robin-schumacher.como>
2023-07-10 09:49:18 +0200

    run2

--- src/main/java/de/uni_mannheim/informatik/dws/t2k/match/components/DuplicateBasedSchemaMatching.java
+++ src/main/java/de/uni_mannheim/informatik/dws/t2k/match/components/DuplicateBasedSchemaMatching.java
@@ -78,13 +78,13 @@ public class DuplicateBasedSchemaMatching {
         this.finalPropertySimilarityThreshold = finalPropertySimilarityThreshold;
     }
 
-    private SimilarityMeasure<String> stringSimilarity = new GeneralisedStringJaccard(new LevenshteinSimilarity(), 0.5, 0.5);
+    private SimilarityMeasure<String> stringSimilarity = new GeneralisedStringJaccard(new LevenshteinSimilarity(), 0.4, 0.4);
 
     private SimilarityMeasure<Double> numericSimilarity = new DeviationSimilarity();
 
     private WeightedDateSimilarity dateSimilarity = new WeightedDateSimilarity(1, 3, 5);
 
-    private int numVotesPerValue = 0;
+    private int numVotesPerValue = 1;
 
     private int numCorrespondencesPerColumn = 3;
 
@@ -134,7 +134,7 @@ public class DuplicateBasedSchemaMatching {
 
         // after aggregation, the best 3 schema correspondences for each attribute on the lhs are created
 
-        schemaCorrespondences = matchingEngine.getTopKSchemaCorrespondences(schemaCorrespondences, numCorrespondencesPerColumn, 0.0);
+        //  schemaCorrespondences = matchingEngine.getTopKSchemaCorrespondences(schemaCorrespondences, numCorrespondencesPerColumn, 0.0);
 
         return schemaCorrespondences;
     }
